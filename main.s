093f3212dd9f956073ac5fc4adca493789e713a1
Neso Hiroshi <122040999+neso-hiroshi@users.noreply.github.com>
2026-06-24 12:26:13 +0500

    Update main.s

--- main.s
+++ main.s
@@ -1,17 +1,21 @@
-.global _main
-.align 2
+format ELF64 object
+processor cpu64_v8
 
-_main:
-    ; 1. Подготовка аргументов для write(1, msg, 14)
+public start as '_main'
+
+segment readable executable
+align 4
+
+start:
+    ; 1. Подготовка аргументов для write(1, msg, 19)
     mov     x0, #1                  ; Куда: stdout (1)
-    adrp    x1, msg@PAGE            ; Что: адрес строки (страница)
-    add     x1, x1, msg@PAGEOFF     ; Смещение строки
-    mov     x2, #14                 ; Сколько байт: длина строки
+    adr     x1, msg                 ; Что: адрес строки (в FASMARM используем adr)
+    mov     x2, #19                 ; Сколько байт: длина строки
 
     ; 2. Системный вызов write
     mov     x16, #4                 ; Номер вызова write = 4
     movk    x16, #0x0200, lsl #16   ; Накладываем маску класса BSD (0x02000000)
-    svc     #0x80                   ; Вызов ядра (в macOS используется svc #0x80)
+    svc     #0x80                   ; Вызов ядра в macOS
 
     ; 3. Системный вызов exit(0)
     mov     x0, #0                  ; Код возврата = 0
@@ -19,5 +23,5 @@ _main:
     movk    x16, #0x0200, lsl #16   ; Накладываем маску класса BSD (0x02000000)
     svc     #0x80                   ; Вызов ядра
 
-.data
-msg: .ascii "Hello, macOS!\n"
+segment readable writeable
+msg db 'Hello, macOS ARM64!', 10
